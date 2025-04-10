f0f897e9d6a1464d840456b07c06a4faee625abf
Tskar <sanskarthapaofficial@gmail.com>
2025-04-10 17:38:04 -0400

    ScreenCaptures & minor tweak.

--- src/components/Report/Report.jsx
+++ src/components/Report/Report.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { generateReport } from "../../services/reportService";
 
-const Report = ({ report }) => {
+const Report = ({report}) => {
   return (
     <div className="bg-white h-100   p-6 rounded-lg shadow-md">
       <h4 className="text-center text-gray-500 mb-4">RECORD</h4>
       <div className="max-h-80 overflow-y-auto">
-        <textarea value={report} readOnly rows={10} cols={50} />
+        <textarea value={report} readOnly rows={50} cols={60} />
       </div>
     </div>
   );
